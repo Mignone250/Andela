c86b0aee91bcff84e124e7585ad9c45754b86e50
Mignonne <unguyenezamignonne@akilahinstitute.org>
2019-10-04 07:36:49 +0200

    ch(Teamwork_Project)Creating article for the user[168944255]

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,12 @@
 const blog = {
+    articles:[
+        {
+            id:1,
+            title: 'Are You Mom Enough?',
+            content: 'This fast-paced era synonymies with  and , enhancement in technology levels has served as a boon for business houses. As a result of such modernized practices, there exists cut throat competition among firms that triggers them to opt for tremendous ways to come out of such perilous situation in a remarkable manner. Internet technology offers an enormous number of advantages to those Institutions who wish to secure a prominent place in the global market. Possessing a website is nothing extraordinary, what makes your venture stand out from the rest is the care and value you transmit to your clients',
+        },
+    
+    ],
     users: [
         {
             id:1,
--- server/routes/Paths.js
+++ server/routes/Paths.js
@@ -1,5 +1,6 @@
 import  express from 'express';
 import Users from '../controllers/Users_api';
+import Articles from '../controllers/Articles_api';
 
 
 
@@ -7,4 +8,5 @@ const router = express();
 
 router.post('/api/V1/auth/userSignup/', Users.addUser);
 router.post('/api/V1/auth/signin', Users.login);
+router.post('/api/V1/articles', Articles.CreateArticle);
 export default router
