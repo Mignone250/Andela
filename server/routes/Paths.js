a606d38d61dd105cefd597976626853825ac2d35
Mignone250 <52957559+Mignone250@users.noreply.github.com>
2019-10-04 05:37:22 +0200

    Revert "ft(Teamwork_Project) Creating account endpoint. The API that will hel…"

--- server/routes/Paths.js
+++
@@ -1,9 +0,0 @@
-import  express from 'express';
-import Users from '../controllers/Users_api';
-
-
-
-const router = express();
-
-router.post('/api/V1/auth/userSignup/', Users.addUser);
-export default router
