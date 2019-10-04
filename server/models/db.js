f2fd991fdcb5ff8d1a50a91c1bbed66cf38b06c1
Mignonne <unguyenezamignonne@akilahinstitute.org>
2019-10-04 03:53:18 +0200

    ft(Teamwork_Project) Creating account endpoint. The API that will help the user to create account[168941701]

---
+++ server/models/db.js
@@ -0,0 +1,20 @@
+const blog = {
+    users: [
+        {
+            id:1,
+            firstName: 'Mignone',
+            lastName: 'Unguyeneza',
+            email: 'MignoneUnguyeneza@gmail.com',
+            gender: 'female',
+            jobRole: 'developer',
+            department: 'Information Systems',
+            address: 'Kanombe',
+            username:'mimi',
+            password: '12345r6t',
+    
+        },
+    ],
+    }
+    
+    export default blog
+    
